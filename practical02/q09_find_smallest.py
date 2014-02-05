c06b44886ab4e2e374383a32d7117efb4b38fc29
Chen Ning <teo.chenning@dhs.sg>
2014-02-05 11:05:33 +0800

    Update q09_find_smallest.py

--- practical02/q09_find_smallest.py
+++ practical02/q09_find_smallest.py
@@ -1,10 +1,4 @@
 #9 Finding the smallest number such that number^2 > 12000
 
-n = 1
-test = False
-while test == False:
-    if n**2 > 12000:
-        print(n)
-        test = True
-    else:
-        n = n + 1
+import math
+print (math.ceil(12000**(1/2.0)))
