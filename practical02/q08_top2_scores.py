35b9e2f0212d68320b47b6179433f436cbba8526
Chen Ning <teo.chenning@dhs.sg>
2014-02-05 10:49:07 +0800

    Update q08_top2_scores.py

--- practical02/q08_top2_scores.py
+++ practical02/q08_top2_scores.py
@@ -12,7 +12,7 @@ while number > len(namelist):
     scorelist.append(score)
 
 first = scorelist.index(max(scorelist))
-second = scorelist.index(max(score for score in scorelist if score <= max(scorelist)))
+second = scorelist.index(max(score for score in scorelist if score < max(scorelist)))
 
 print("{0} has the highest score with {1}.".format(namelist[first], scorelist[first])) 
 print("{0} has the second highest score with {1}.".format(namelist[second], scorelist[second]))
